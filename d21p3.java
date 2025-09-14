fa9d4b7efd548a054e05e2295044490e9f304e2f
ishraqmansuri <ishraqmansuri@gmail.com>
2025-09-14 18:42:43 +0530

    newstr to newStr

--- d21p3.java
+++ d21p3.java
@@ -1,21 +1,21 @@
 public class d21p3 {
     
 
-    public static void allseq(String str , int idx , String newStr) {
+    public static void allSeq(String str , int idx , String newStr) {
       if (idx == str.length()) {
             System.out.println(newStr);
             return;
       }
       char mainchar = str.charAt(idx);
 
-      allseq(str, idx+1, newStr+mainchar);
+      allSeq(str, idx+1, newStr+mainchar);
 
-      allseq(str, idx+1, newStr);
+      allSeq(str, idx+1, newStr);
       
 
     }
 
     public static void main(String[] args) {
-       allseq("abc",0, "");
+       allSeq("abc",0, "");
     }
 }
